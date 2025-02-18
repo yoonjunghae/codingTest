2ecf21189f922ff0a875416eb89accb71540a181
YoonJungHae <yoongomtang@naver.com>
2025-02-18 20:35:30 +0900

    1-09 숫자만 추출 : Character.isDigit(x)

--- src/sec1/Main11.java
+++ src/sec1/Main11.java
@@ -3,19 +3,19 @@ package sec1;
 import java.util.Scanner;
 
 //숫자만 추출
-public class Main10 {
+public class Main11 {
     public int solution(String s){
-        int answer = 0;
-        for (char x :s.toCharArray() ) {
-            if(x>=48 && x<=57) answer = answer*10 + (x-48);
+        String answer = "";
+        for (char x :s.toCharArray()) {
+            if(Character.isDigit(x)) answer+=x;
 
         }
 
-        return answer;
+        return Integer.parseInt(answer);
     }
 
     public static void main(String[] args) {
-        Main10 t = new Main10();
+        Main11 t = new Main11();
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
         System.out.println(t.solution(str));
